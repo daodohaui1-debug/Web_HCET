98354a8b6eefa7c3353fb7c3a68f29f97b9080a1
dongoc09092002 <ngocdo09092002@gmail.com>
2026-02-13 16:47:41 +0700

    add e

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBneuFOu5I89H-01xvMZFlMj71yYjg0zQI",
-    authDomain: "hoccunget.firebaseapp.com",
-    projectId: "hoccunget",
-    storageBucket: "hoccunget.firebasestorage.app",
-    messagingSenderId: "430560952804",
-    appId: "1:430560952804:web:897d9f2381e41046bd87f1",
-    measurementId: "G-6SNKC725HR"
+  apiKey: "AIzaSyC3THpTAz--ywaNyz3nQm54OPADsWVUljc",
+  authDomain: "hoccunget.firebaseapp.com",
+  projectId: "hoccunget",
+  storageBucket: "hoccunget.firebasestorage.app",
+  messagingSenderId: "430560952804",
+  appId: "1:430560952804:web:897d9f2381e41046bd87f1",
+  measurementId: "G-6SNKC725HR",
 };
 
 // Initialize Firebase
